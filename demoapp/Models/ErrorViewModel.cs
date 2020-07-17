5ab338012b6b4f15cd835495d1130533e9647a8a
Hem Acharya <hem.acharya@perficient.com>
2020-07-16 22:40:10 -0400

    Add project files.

---
+++ demoapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace demoapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
